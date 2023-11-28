056d85177cdae6f5886dd4fd8c445f2644c54c72
Mai <mai.iam2048@gmail.com>
2023-11-28 21:34:23 +0100

    Merge pull request #12122 from JosJuice/jit-imm-msr
    Jit: Handle imm msr in EmitStoreMembase

--- Source/Core/Core/PowerPC/JitArm64/Jit.h
+++ Source/Core/Core/PowerPC/JitArm64/Jit.h
@@ -310,6 +310,7 @@ protected:
   void EndTimeProfile(JitBlock* b);
 
   void EmitUpdateMembase();
+  void EmitStoreMembase(u32 msr);
   void EmitStoreMembase(const Arm64Gen::ARM64Reg& msr);
 
   // Exits
