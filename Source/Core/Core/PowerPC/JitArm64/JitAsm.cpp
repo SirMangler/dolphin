0c80a740de411e49b0ec9885f72c0d9622964c48
Mai <mai.iam2048@gmail.com>
2023-11-28 04:19:27 +0100

    Merge pull request #12231 from JosJuice/jitarm64-frsqrte-optimization
    JitArm64: Optimize frsqrte routine

--- Source/Core/Core/PowerPC/JitArm64/JitAsm.cpp
+++ Source/Core/Core/PowerPC/JitArm64/JitAsm.cpp
@@ -340,8 +340,8 @@ void JitArm64::GenerateFrsqrte()
   LDP(IndexType::Signed, ARM64Reg::W3, ARM64Reg::W2, ARM64Reg::X2, 0);
   UBFX(ARM64Reg::X1, ARM64Reg::X1, 37, 11);
   AND(ARM64Reg::X0, ARM64Reg::X0, LogicalImm(Common::DOUBLE_SIGN | Common::DOUBLE_EXP, 64));
-  MADD(ARM64Reg::W3, ARM64Reg::W3, ARM64Reg::W2, ARM64Reg::W1);
-  ORR(ARM64Reg::X0, ARM64Reg::X0, ARM64Reg::X3, ArithOption(ARM64Reg::X3, ShiftType::LSL, 26));
+  MADD(ARM64Reg::W1, ARM64Reg::W1, ARM64Reg::W2, ARM64Reg::W3);
+  ORR(ARM64Reg::X0, ARM64Reg::X0, ARM64Reg::X1, ArithOption(ARM64Reg::X1, ShiftType::LSL, 26));
   RET();
 
   SetJumpTarget(zero);
